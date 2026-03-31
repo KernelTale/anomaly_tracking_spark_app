2c03bee634552b8dcbb4aa6c4c05d35bd566a153
KernelTale <58466615+KernelTale@users.noreply.github.com>
2026-03-31 00:56:28 -0700

    Update month_request.py

--- month_request.py
+++ month_request.py
@@ -1,4 +1,3 @@
-> Sir:
 #import dum' stuff as schemas an' dataframe --> python data structures transitions
 import os
 import uuid
@@ -24,19 +23,19 @@ from dependencies.io_spark_ueba_anomalies import read_from_db
 
 @udf(returnType=StringType())
 def get_uuid():
-return str(uuid.uuid4())
+  return str(uuid.uuid4())
 
 spark = (SparkSession.builder
-.appName("test_UEBA_anomalies_match_modules")
-.config("spark.dynamicAllocation.enabled", "true")
-.config("spark.executor.memory", "6g")
-.config("spark.driver.memory", "6g")
-.config("spark.executor.cores", "1")
-.config("spark.decommission.enabled", "false") 
-.config('spark.sql.legacy.timeParserPolicy', 'LEGACY')
-.config("spark.sql.session.timeZone", "Europe/Moscow")
-.master("local[*]")
-.getOrCreate())
+          .appName("test_UEBA_anomalies_match_modules")
+          .config("spark.dynamicAllocation.enabled", "true")
+          .config("spark.executor.memory", "6g")
+          .config("spark.driver.memory", "6g")
+          .config("spark.executor.cores", "1")
+          .config("spark.decommission.enabled", "false") 
+          .config('spark.sql.legacy.timeParserPolicy', 'LEGACY')
+          .config("spark.sql.session.timeZone", "Europe/Moscow")
+          .master("local[*]")
+          .getOrCreate())
 anomalies_raw = get_anomalies_file('./configs/import_anomalies_table.csv')
 db_properties_realtime = get_json_conf_file('./configs/request_db_conf_realtime.json')
 db_properties_month = get_json_conf_file('./configs/request_db_conf_month.json')
@@ -49,41 +48,41 @@ spark = fill_catalogue_namespace(db_properties_month, spark, suffix=suffix, ssl_
 # MATCH INDEXES WITH ANOMALY CLASSES 
 anomalies_class_pool = dict()
 for idx in anomalies_raw:
-anomaly_class_id = (anomalies_raw.get(idx)).get("anomaly_class")
-if anomaly_class_id:
-if anomaly_class_id in anomalies_class_pool:
-(anomalies_class_pool.get(anomaly_class_id)).append(int(idx))
-else:
-anomalies_class_pool.update({anomaly_class_id : list([int(idx)])})
+  anomaly_class_id = (anomalies_raw.get(idx)).get("anomaly_class")
+  if anomaly_class_id:
+    if anomaly_class_id in anomalies_class_pool:
+      (anomalies_class_pool.get(anomaly_class_id)).append(int(idx))
+    else:
+      anomalies_class_pool.update({anomaly_class_id : list([int(idx)])})
 
 # READ INTO DATAFRAMES
 for anomaly_class_id in anomalies_class_pool:
-#connection goes at this loop
-req_fields = [(anomalies_raw.get(idx)).get("req_fields") for idx in anomalies_class_pool.get(anomaly_class_id)]
-req_fields = set([field for liste in req_fields for field in liste])
-#gathering fields among the anomaly class
-fields = [(anomalies_raw.get(idx)).get("fields") for idx in anomalies_class_pool.get(anomaly_class_id)]
-fields = set([field for liste in fields for field in liste])
-#goin through several databases those are bound to that particular anomaly class
-for db_properties_prod in db_properties_realtime.get(anomaly_class_id):
-props = {"prod_name": db_properties_prod,
-"db_properties_realtime": (db_properties_realtime.get(anomaly_class_id)).get(db_properties_prod),
-"req_fields": req_fields,
-"catalogue_id": ((db_properties_realtime.get(anomaly_class_id)).get(db_properties_prod)).get("database") + suffix,
-"one_month_only": True
-}
-df_one_month = read_from_db(props, spark)
-if db_properties_month.get(anomaly_class_id):
-if db_properties_month.get(anomaly_class_id).get("Universal"):
-props.update({ "sup_catalogue_id": ((db_properties_month.get(anomaly_class_id)).get("Universal")).get("database") + suffix,
-"db_properties_month": (db_properties_month.get(anomaly_class_id).get("Universal"))
-})
-
-print(props.get("sup_catalogue_id") + "." + (props.get("db_properties_month").get("database"))\
-+ "." + (list(((props.get("db_properties_month")).get("tables")).keys()))[0])
-df_one_month.withColumn("uuid", get_uuid()).withColumnRenamed("first_occurrence", "timestamp").writeTo(props.get("sup_catalogue_id")\
-+ "." + (props.get("db_properties_month").get("database"))\
-+ "." + (list(((props.get("db_properties_month")).get("tables")).keys()))[0]).append()
-df_one_month.withColumn("uuid", get_uuid()).withColumnRenamed("first_occurrence", "timestamp").show(3)
-#df_one_month.withColumnRenamed("first_occurrence", "timestamp").show(5)
+  #connection goes at this loop
+  req_fields = [(anomalies_raw.get(idx)).get("req_fields") for idx in anomalies_class_pool.get(anomaly_class_id)]
+  req_fields = set([field for liste in req_fields for field in liste])
+  #gathering fields among the anomaly class
+  fields = [(anomalies_raw.get(idx)).get("fields") for idx in anomalies_class_pool.get(anomaly_class_id)]
+  fields = set([field for liste in fields for field in liste])
+  #goin through several databases those are bound to that particular anomaly class
+  for db_properties_prod in db_properties_realtime.get(anomaly_class_id):
+    props = {"prod_name": db_properties_prod,
+    "db_properties_realtime": (db_properties_realtime.get(anomaly_class_id)).get(db_properties_prod),
+    "req_fields": req_fields,
+    "catalogue_id": ((db_properties_realtime.get(anomaly_class_id)).get(db_properties_prod)).get("database") + suffix,
+    "one_month_only": True
+    }
+    df_one_month = read_from_db(props, spark)
+    if db_properties_month.get(anomaly_class_id):
+      if db_properties_month.get(anomaly_class_id).get("Universal"):
+        props.update({ "sup_catalogue_id": ((db_properties_month.get(anomaly_class_id)).get("Universal")).get("database") + suffix,
+        "db_properties_month": (db_properties_month.get(anomaly_class_id).get("Universal"))
+        })
+        
+        print(props.get("sup_catalogue_id") + "." + (props.get("db_properties_month").get("database"))\
+        + "." + (list(((props.get("db_properties_month")).get("tables")).keys()))[0])
+        df_one_month.withColumn("uuid", get_uuid()).withColumnRenamed("first_occurrence", "timestamp").writeTo(props.get("sup_catalogue_id")\
+        + "." + (props.get("db_properties_month").get("database"))\
+        + "." + (list(((props.get("db_properties_month")).get("tables")).keys()))[0]).append()
+        df_one_month.withColumn("uuid", get_uuid()).withColumnRenamed("first_occurrence", "timestamp").show(3)
+        #df_one_month.withColumnRenamed("first_occurrence", "timestamp").show(5)
 df_one_month.drop()
